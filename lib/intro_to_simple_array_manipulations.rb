10365568524c0319fc47c3f4b42bccd9dde75756
Maharaj Syed <maharajsyed@yahoo.com>
2020-02-26 23:07:44 +0000

    Done.

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,59 @@
+#push method
+def using_push(array,string)
+  array.push(string)
+end
+
+#unshift method
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+#pop method
+def using_pop(array)
+  array.pop
+end
+
+#pop with args method
+def pop_with_args(array)
+  array.pop(2)
+end
+
+#shift method
+def using_shift(array)
+  array.shift
+end
+
+#shift with args method
+def shift_with_args(array)
+  array.shift(2)
+end
+
+#using concat
+def using_concat(my_favorite_fruits, more_fruits)
+  my_favorite_fruits.concat(more_fruits)
+end
+
+#using insert 
+def using_insert(my_favorite_fruits, more_fruits)
+  my_favorite_fruits.insert(4, more_fruits)
+end
+
+#using uniq
+def using_uniq(my_favorite_fruits)
+  my_favorite_fruits.uniq
+end
+
+#using flatten
+def using_flatten(my_favorite_fruits)
+  my_favorite_fruits.flatten
+end
+
+#using delete
+def using_delete(my_favorite_fruits, papaya)
+  my_favorite_fruits.delete(papaya)
+end
+
+#using delete at
+def using_delete_at(my_favorite_fruits, integer)
+  my_favorite_fruits.delete_at(integer)
+end
