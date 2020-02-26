e94cda3d74cbf60c6c2e75d2dbb2d0c7d1d9952b
Maharaj Syed <maharajsyed@yahoo.com>
2020-02-26 21:25:29 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,40 @@
+#push method
+def using_push(array,string)
+  array.push(string)
+end
+
+#unshift method
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+#pop method
+def using_pop(array)
+  array.pop
+end
+
+#pop with args method
+def pop_with_args(array)
+  array.pop(2)
+end
+
+#shift method
+def using_shift(array)
+  array.shift
+end
+
+#shift with args method
+def shift_with_args(array)
+  array.shift(2)
+end
+
+#using concat
+def using_concat(my_favorite_fruits, more_fruits)
+  my_favorite_fruits.concat(more_fruits)
+end
+
+#using_concat(my_favorite_fruits, more_fruits)
+#using insert 
+
+
+#
